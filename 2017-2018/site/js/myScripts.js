028f7878ab26a151bf296b9ad4c6404507c37723
Alexandre Ducobu <Harchytekt@users.noreply.github.com>
2018-05-05 17:21:56 +0200

    :sparkles: Add event when leaving the 'french' word

--- 2017-2018/site/js/myScripts.js
+++ 2017-2018/site/js/myScripts.js
@@ -20,7 +20,6 @@ $(document).ready(function() {
     var offset = 220;
     var duration = 1000;
     $(this).scroll(function() {
-        console.log("hgfhdjgf");
         if ($(this).scrollTop() > offset) {
             console.log($(this).scrollTop());
             $('.back-to-top').fadeIn(duration);
@@ -33,6 +32,31 @@ $(document).ready(function() {
         event.preventDefault();
         $('html, body').animate({scrollTop: 0}, duration);
         return false;
-    })
+    });
 
 });
+
+/* French Event Start */
+
+var nbClicks = 0, fr = $('#french').html(), tempFr, res, tempIndex;
+//$(document).on('mouseleave touchstart', '#french', function() { // For mobile
+$(document).on('mouseout', '#french', function() {
+	$(this).toggle('puff');
+
+	if (nbClicks++ % 2 == 0) {
+		tempFr = fr.split('');
+		res = [];
+		while (tempFr.length > 0) {
+			tempIndex = Math.floor(Math.random() * tempFr.length);
+			res.push(tempFr[tempIndex]);
+			tempFr.splice(tempIndex, 1);
+		}
+		$(this).html(res.join(''));
+	} else {
+		$(this).html(fr);
+	}
+
+	$(this).toggle('puff');
+});
+
+/* French Event End */
