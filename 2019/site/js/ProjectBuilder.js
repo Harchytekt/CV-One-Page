a65b441b62763ef057f0a465d0bda89d5a830902
Alexandre Ducobu <alexandre.ducobu@yahoo.be>
2019-07-27 17:31:42 +0200

    :art: Improve the project builder

--- 2019/site/js/ProjectBuilder.js
+++ 2019/site/js/ProjectBuilder.js
@@ -1,17 +1,86 @@
-var definitions;
-var definitionsJSON = $.getJSON("json/definitions.json", function() {
-	// console.log(`success`);
-})
-.done(function(json) {
-	definitions = jQuery.parseJSON(JSON.stringify(json.definitions));
+var currentProject;
 
-	var jqxhr = $.getJSON("json/projects.json", function() {
-		// console.log(`success`);
-	})
-	.done(function(json) {
-		buildProjects(json.projects);
-	});
-});
+function addOneProject(project) {
+	currentProject = project;
+	return `
+		<!-- Start project's summary -->
+		${ addSummary() }
+		<!-- End project's summary -->
+		<!-- Start tech -->
+		<div class="tech" hidden>${ addTechs() }</div>
+		<!-- End tech -->
+		<br>
+		<!-- Start links -->
+		<div class="card-links ${ addLinks() }
+		</div>
+		<!-- End links -->`;
+}
+
+function addSummary() {
+	var summaryHtml = ``;
+	if (currentProject.isForSchool !== undefined && currentProject.isForSchool) {
+		summaryHtml += `<img src="resources/img/school.svg" class="svg school" height="64" title="Projet scolaire">`;
+	}
+
+	summaryHtml += `
+		<h5 class="card-title" title="${ currentProject.period !== undefined ? currentProject.period : currentProject.title }">${ currentProject.title }</h5>
+		<div class="desc">
+			<p class="card-text">
+			<sup class="period">${currentProject.period}</sup> <br>`;
+
+	for (var j = 0; j < currentProject.description.length; j++) {
+		summaryHtml += `${ currentProject.description[j].line }`;
+		if (j < currentProject.description.length - 1) {
+			summaryHtml += `<br>`;
+		}
+	}
+	return summaryHtml += `</p></div>`;
+}
+
+function addTechs() {
+	var techHtml = ``;
+	if (currentProject.subtitle !== undefined) {
+		techHtml += `<h6 class="card-subtitle mb-2 text-muted">${ currentProject.subtitle }</h6>`;
+	}
+	techHtml += `<p class="card-text role">`;
+	if (currentProject.role !== undefined) {
+		techHtml += `<strong>Rôle :</strong> <br>`;
+		for (var j = 0; j < currentProject.role.length; j++) {
+			techHtml += `${ currentProject.role[j].name }`;
+			if (j < currentProject.role.length -1) {
+				techHtml += `, `;
+			}
+		}
+		techHtml += ` <br>`;
+	}
+	return techHtml + `</p>
+				<p class="card-text">
+					${ addOneTech('Environnement ', currentProject.env) }
+					${ addOneTech('Frameworks ', currentProject.framework) }
+					${ addOneTech('Méthodologie ', currentProject.meth) }
+					${ addOneTech('Moteur de production ', currentProject.build) }
+					${ addOneTech('Outils ', currentProject.tool) }
+					${ addOneTech('Autres ', currentProject.other) }
+				</p>`;
+}
+
+function addOneTech(techName, techList) {
+	var currentTechHtml = ``;
+	if (techList !== undefined) {
+		currentTechHtml += `<strong>${ techName }:</strong> <br>`;
+		for (var j = 0; j < techList.length; j++) {
+			currentTechHtml += `
+							<span class="defTooltip" data-toggle="tooltip" data-html="true" title="${ findDefinition(techList[j].name) }">
+								${ techList[j].name }
+							</span>`;
+			if (j < techList.length -1) {
+				currentTechHtml += `, `;
+			}
+		}
+		currentTechHtml += ` <br>`;
+	}
+	return currentTechHtml;
+}
 
 function findDefinition(name) {
 	for (var def = 0; def < definitions.length; def++) {
@@ -22,247 +91,74 @@ function findDefinition(name) {
 	return "Aucune définition trouvée";
 }
 
-function buildProjects(projects) {
-	var projectsHtml = ``;
-	for (var i = 0; i < projects.length; i++) {
-		var project = projects[i].project;
-		projectsHtml += `<!-- ${ project.title } -->`;
-		projectsHtml += `
-		<div class="grid-item align-self-center">
-			<div class="card">
-				<div class="card-body">
-		`;
-		if (project.isForSchool !== undefined && project.isForSchool) {
-			projectsHtml += `<img src="resources/img/school.svg" class="svg school" height="64" title="Projet scolaire">`;
-		}
-		// Add title
-		projectsHtml += `<h5 class="card-title" title="${ project.period !== undefined ? project.period : project.title }">${ project.title }</h5>`;
-		// Add desc
-		projectsHtml += `
-					<div class="desc">
-						<p class="card-text">
-							<sup class="period">${project.period}</sup> <br>`;
-		for (var j = 0; j < project.description.length; j++) {
-			projectsHtml += `${ project.description[j].line }`;
-			if (j < project.description.length - 1) {
-				projectsHtml += `<br>`;
-			}
-		}
-		projectsHtml += `
-						</p>
-					</div>
+function addLinks() {
+	var linksHtml = ``;
+	if (currentProject.links === undefined) {
+		linksHtml += `
+			noLink">
+				<span class="card-link more">+</span>`;
+	} else {
+		if (currentProject.links.length === 2) {
+			linksHtml += `
+			links">`;
+			for (var link = 0; link < currentProject.links.length; link++) {
+				if (currentProject.links[link].type === 'git') {
+					linksHtml += `
+				${ addOneLink('git', currentProject.links[link].url) }
+				<span class="card-link more">+</span>
 					`;
-		// Add tech
-		projectsHtml += `<div class="tech" hidden>`;
-		if (project.subtitle !== undefined) {
-			projectsHtml += `<h6 class="card-subtitle mb-2 text-muted">${ project.subtitle }</h6>`;
-		}
-		projectsHtml += `<p class="card-text role">`;
-		if (project.role !== undefined) {
-			projectsHtml += `<strong>Rôle :</strong> <br>`;
-			for (var j = 0; j < project.role.length; j++) {
-				projectsHtml += `${ project.role[j].role }`;
-				if (j < project.role.length -1) {
-					projectsHtml += `, `;
+				}
+				if (currentProject.links[link].type === 'report') {
+					linksHtml += `${ addOneLink('report', currentProject.links[link].url) }`;
+				} else if (currentProject.links[link].type === 'site') {
+					linksHtml += `${ addOneLink('site', currentProject.links[link].url) }`;
 				}
 			}
-			projectsHtml += ` <br>`;
-		}
-		projectsHtml += `</p>
-						<p class="card-text">
-						`;
-		if (project.env !== undefined) {
-			projectsHtml += `<strong>Environnement :</strong> <br>`;
-			for (var j = 0; j < project.env.length; j++) {
-				projectsHtml += `<span class="defTooltip" data-toggle="tooltip" data-html="true" title="${findDefinition(project.env[j].env)}">
-									${ project.env[j].env }
-								</span>`;
-				if (j < project.env.length -1) {
-					projectsHtml += `, `;
-				}
-			}
-			projectsHtml += ` <br>`;
-		}
-		if (project.framework !== undefined) {
-			projectsHtml += `<strong>Frameworks :</strong> <br>`;
-			for (var j = 0; j < project.framework.length; j++) {
-				projectsHtml += `<span class="defTooltip" data-toggle="tooltip" data-html="true" title="${findDefinition(project.framework[j].framework)}">
-									${ project.framework[j].framework }
-								</span>`;
-				if (j < project.framework.length - 1) {
-					projectsHtml += `, `;
-				}
-			}
-			projectsHtml += ` <br>`;
-		}
-		if (project.meth !== undefined) {
-			projectsHtml += `<strong>Méthodologie :</strong> <br>`
-			for (var j = 0; j < project.meth.length; j++) {
-				projectsHtml += `<span class="defTooltip" data-toggle="tooltip" data-html="true" title="${findDefinition(project.meth[j].meth)}">
-									${ project.meth[j].meth }
-								</span>`;
-				if (j < project.meth.length - 1) {
-					projectsHtml += `, `;
-				}
-			}
-			projectsHtml += ` <br>`;
-		}
-		if (project.build !== undefined) {
-			projectsHtml += `<strong>Moteur de production :</strong> <br>`
-			for (var j = 0; j < project.build.length; j++) {
-				projectsHtml += `<span class="defTooltip" data-toggle="tooltip" data-html="true" title="${findDefinition(project.build[j].build)}">
-									${ project.build[j].build }
-								</span>`;
-				if (j < project.build.length - 1) {
-					projectsHtml += `, `;
-				}
-			}
-			projectsHtml += ` <br>`;
-		}
-		if (project.tool !== undefined) {
-			projectsHtml += `<strong>Outils :</strong> <br>`;
-			for (var j = 0; j < project.tool.length; j++) {
-				projectsHtml += `<span class="defTooltip" data-toggle="tooltip" data-html="true" title="${findDefinition(project.tool[j].tool)}">
-									${ project.tool[j].tool }
-								</span>`;
-				if (j < project.tool.length - 1) {
-					projectsHtml += `, `;
-				}
-			}
-			projectsHtml += ` <br>`;
-		}
-		if (project.other !== undefined) {
-			projectsHtml += `<strong>Autres :</strong> <br>`;
-			for (var j = 0; j < project.other.length; j++) {
-				projectsHtml += `<span class="defTooltip" data-toggle="tooltip" data-html="true" title="${findDefinition(project.other[j].other)}">
-									${ project.other[j].other }
-								</span>`;
-				if (j < project.other.length - 1) {
-					projectsHtml += `, `;
-				}
-			}
-			projectsHtml += ` <br>`;
-		}
-		projectsHtml += `
-						</p>
-					</div>
-		`;
-		// Add links
-		projectsHtml += `
-					<br>
-					<div class="card-links `;
-
-		if (project.links === undefined) {
-			projectsHtml += `noLink">
-							<span class="card-link more">+</span>`;
+		} else if (currentProject.links[0].type === 'git') {
+			linksHtml += `
+			links">
+				${ addOneLink('git', currentProject.links[0].url) }
+				<span class="card-link more">+</span>
+				<span class="card-link pjt hidden"></span>
+			`;
 		} else {
-			if (project.links.length === 2) {
-				projectsHtml += `links">`;
-				for (var link = 0; link < project.links.length; link++) {
-					if (project.links[link].type === "git") {
-						projectsHtml += `
-								<a href="${ project.links[link].url }" class="card-link pjt" title="Voir sur git" target="_blank">
-									<img src="resources/img/git.svg" class="svg" height="20"> Projet git
-								</a>
-								<span class="card-link more">+</span>
-						`;
-					}
-					if (project.links[link].type === "report") {
-						projectsHtml += `
-									<a href="${ project.links[link].url }" class="card-link rpt" title="Voir le rapport" target="_blank">
-										<img src="resources/img/report.svg" class="svg" height="20"> Rapport
-									</a>
-						`;
-					} else if (project.links[link].type === "site") {
-						projectsHtml += `
-									<a href="${ project.links[link].url }" class="card-link site" title="Voir le site" target="_blank">
-										<img src="resources/img/link.svg" class="svg" height="20"> Site web
-									</a>
-						`;
-					}
-				}
-			} else if (project.links[0].type === "git") {
-				projectsHtml += `links">
-									<a href="${ project.links[0].url }" class="card-link pjt" title="Voir sur git" target="_blank">
-										<img src="resources/img/git.svg" class="svg" height="20"> Projet git
-									</a>
-									<span class="card-link more">+</span>
-									<span class="card-link pjt hidden"></span>
-				`;
-			} else {
-				projectsHtml += `links">
-									<span class="card-link pjt hidden"></span>
-									<span class="card-link more">+</span>`;
-				if (project.links[0].type === "report") {
-					projectsHtml += `
-									<a href="${ project.links[0].url }" class="card-link rpt" title="Voir le rapport" target="_blank">
-										<img src="resources/img/report.svg" class="svg" height="20"> Rapport
-									</a>
-					`;
-				} else if (project.links[0].type === "site") {
-					projectsHtml += `
-									<a href="${ project.links[0].url }" class="card-link site" title="Voir le site" target="_blank">
-										<img src="resources/img/link.svg" class="svg" height="20"> Site web
-									</a>
-					`;
-				}
+			linksHtml += `
+			links">
+				<span class="card-link pjt hidden"></span>
+				<span class="card-link more">+</span>`;
+			if (currentProject.links[0].type === 'report') {
+				linksHtml += `${ addOneLink('report', currentProject.links[0].url) }`;
+			} else if (currentProject.links[0].type === 'site') {
+				linksHtml += `${ addOneLink('site', currentProject.links[0].url) }`;
 			}
-			
 		}
-
-		projectsHtml += `
-					</div>
-				</div>
-			</div>
-		</div>
-		`;
 	}
-	$('.grid-container.projects').html(projectsHtml);
-	toSvg();
-	$('[data-toggle="tooltip"]').tooltip();
+	return linksHtml;
 }
 
-/*
- * Replace all SVG images with inline SVG
- */
-function toSvg() {
-	$('img.svg').each(function() {
-		var img = $(this);
-		var imgID = img.attr('id');
-		var imgClass = img.attr('class');
-		var imgURL = img.attr('src');
-		var imgHeight = img.attr('height');
-		var imgTitle = img.attr('title');
+function addOneLink(type, link) {
+	var title = ``;
+	var text = ``;
+	var className = ``;
+	var icon = ``;
 
-		$.get(imgURL, function(data) {
-			// Get the SVG tag, ignore the rest
-			var svg = $(data).find('svg');
-
-			// Add replaced image's ID to the new SVG
-			if (typeof imgID !== 'undefined') {
-				svg = svg.attr('id', imgID);
-			}
-			
-			svg = svg.attr('height', imgHeight);
-			svg = svg.attr('width', imgHeight);
-
-			// Add replaced image's classes to the new SVG
-			if (typeof imgClass !== 'undefined') {
-				svg = svg.attr('class', imgClass+' replaced-svg');
-			}
-
-			// Add title
-			/* if (typeof imgTitle !== 'undefined') {
-				//svg = svg.attr('title', imgTitle);
-				svg.append(`<title>test</title>`);
-			} */
-
-			// Remove any invalid XML tags as per http://validator.w3.org
-			svg = svg.removeAttr('xmlns:a');
-
-			// Replace image with new SVG
-			img.replaceWith(svg);
-
-		}, 'xml');
-	});
+	if (type === 'git') {
+		title = `Voir sur git`;
+		text = `Projet git`;
+		className = `pjt`;
+		icon = type;
+	} else if (type === 'report') {
+		title = `Voir le rapport`;
+		text = `Rapport`;
+		className = `rpt`;
+		icon = `report`;
+	} else {
+		title = `Voir le site web`;
+		text = `Site web`;
+		className = `site`;
+		icon = `link`;
+	}
+	return `<a href="${ link }" class="card-link ${ className }" title="${ title }" target="_blank">
+				<img src="resources/img/${ icon }.svg" class="svg" height="20"> ${ text }
+			</a>`;
 }
---
+++ 2019/site/js/ProjectsBuilder.js
@@ -0,0 +1,78 @@
+var definitions;
+var definitionsJSON = $.getJSON("json/definitions.json", function() {
+	// console.log(`success`);
+})
+.done(function(json) {
+	definitions = jQuery.parseJSON(JSON.stringify(json.definitions));
+
+	var jqxhr = $.getJSON("json/projects.json", function() {
+		// console.log(`success`);
+	})
+	.done(function(json) {
+		buildProjects(json.projects);
+	});
+});
+
+function buildProjects(projects) {
+	var projectsHtml = ``;
+	for (var i = 0; i < projects.length; i++) {
+		var project = projects[i].project;
+		projectsHtml += `
+		<!-- ${ project.title } -->
+		<div class="grid-item align-self-center">
+			<div class="card">
+				<div class="card-body">
+					${ addOneProject(project) }
+				</div>
+			</div>
+		</div>`;
+	}
+	$('.grid-container.projects').html(projectsHtml);
+	toSvg();
+	$('[data-toggle="tooltip"]').tooltip();
+}
+
+/*
+ * Replace all SVG images with inline SVG
+ */
+function toSvg() {
+	$('img.svg').each(function() {
+		var img = $(this);
+		var imgID = img.attr('id');
+		var imgClass = img.attr('class');
+		var imgURL = img.attr('src');
+		var imgHeight = img.attr('height');
+		var imgTitle = img.attr('title');
+
+		$.get(imgURL, function(data) {
+			// Get the SVG tag, ignore the rest
+			var svg = $(data).find('svg');
+
+			// Add replaced image's ID to the new SVG
+			if (typeof imgID !== 'undefined') {
+				svg = svg.attr('id', imgID);
+			}
+			
+			svg = svg.attr('height', imgHeight);
+			svg = svg.attr('width', imgHeight);
+
+			// Add replaced image's classes to the new SVG
+			if (typeof imgClass !== 'undefined') {
+				svg = svg.attr('class', imgClass+' replaced-svg');
+			}
+
+			// Add title
+			/* if (typeof imgTitle !== 'undefined') {
+				//svg = svg.attr('title', imgTitle);
+				svg.append(`<title>test</title>`);
+			} */
+
+			// Remove any invalid XML tags as per http://validator.w3.org
+			svg = svg.removeAttr('xmlns:a');
+
+			// Replace image with new SVG
+			img.replaceWith(svg);
+
+		}, 'xml');
+	});
+}
